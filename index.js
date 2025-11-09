1aa14c2180cc58fc7171f3683cefe96c59f9bf77
hriday33333 <zakirhasanhriday@gmail.com>
2025-11-09 07:01:45 +0600

    creatrd get api

--- index.js
+++ index.js
@@ -27,12 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db =client.db('TravelEase-server')
+    const modelCollection =db.collection('models')
+
+  app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
