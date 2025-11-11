ebdb9ecc83beb692d540e63d19454ecc2f4e3bda
hriday33333 <zakirhasanhriday@gmail.com>
2025-11-11 21:21:24 +0600

    created env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,8 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://TravelEase-server:rSnVtJk2pNgWcGmy@cluster0.gisrno5.mongodb.net/?appName=Cluster0';
+const uri = `mongodb+srv://${process.env.DB_USERNANE}:${process.env.DB_PASSWORD}@cluster0.gisrno5.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,16 +31,13 @@ async function run() {
     const bookingsCollection = db.collection('bookings');
 
     // Booking related
-
     // 🔹 Get all bookings (or filter by user email)
     app.get('/bookings', async (req, res) => {
       const email = req.query.email;
       const query = {};
-
       if (email) {
         query.userEmail = email; // লগইন ইউজারের ইমেইল দিয়ে ফিল্টার
       }
-
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
     });
@@ -49,6 +46,12 @@ async function run() {
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -75,6 +78,16 @@ async function run() {
       }
 
       const result = await modelCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/sortmodels', async (req, res) => {
+      const result = await modelCollection
+        .find()
+        .sort({ createdAt: -1 }) // newest first
+        .limit(6) // last 6 items
+        .toArray();
+
       res.send(result);
     });
 
